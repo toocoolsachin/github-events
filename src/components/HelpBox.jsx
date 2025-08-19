96b6f24f38819b88077333c934a383e1cb7774ed
Sachin Kumar <sachin89uk@gmail.com>
2025-08-19 16:47:49 +0530

    upload artifacts

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -4,7 +4,7 @@ import './HelpBox.css';
 
 function HelpBox({ title, text }) {
   return (
-    <article className="help-box">
+    <article className='help-box'>
       <h3>{title}</h3>
       <p>{text}</p>
     </article>
